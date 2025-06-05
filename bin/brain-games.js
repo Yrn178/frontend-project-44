d78c277767c65ba2f6f103fcca689248b95e5ceb
Yrn178 <112001294+Yrn178@users.noreply.github.com>
2025-06-05 16:37:48 +0300

    Add files via upload

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-
-import readlineSyncQuestion from '../src/cli.js';
+import {readlineSyncQuestion} from '../src/cli.js';
 console.log("Welcome to the Brain Games!");
 readlineSyncQuestion(); //Узнаём имя
