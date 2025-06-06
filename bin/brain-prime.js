2069f4c390a683318c18f500614ed7bfd151e9be
Yrn178 <112001294+Yrn178@users.noreply.github.com>
2025-06-06 09:20:02 +0300

    Add files via upload

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -32,6 +32,7 @@ const playGame = (name) => {
     if (answer !== correctAnswer) {
       console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
       console.log(`Let's try again, ${name}!`);
+      return;
     }
     console.log('Correct!');
   }
